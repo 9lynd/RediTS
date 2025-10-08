616955d20ba1e59c7a06f60842de9c7b7ced9907
Ali Mohamed <ali405930@fci.bu.edu.eg>
2025-10-08 14:10:47 +0300

    [add[fix]: set & get expiry]

--- app/commands/set.ts
+++ app/commands/set.ts
@@ -10,12 +10,24 @@ export function setCommand(args: string[]): string {
   const key = args[0];
   const value = args[1];
 
-  if(args[2]) {
-  const expire = parseInt(args[2]);
-  store.set(key, value, new Date(Date.now() + expire));
+  if(args[2] && args[3]) {
+    const option = args[2].toUpperCase();  
+    const expire = parseInt(args[3]);
+    store.set(key, value, new Date(Date.now() + expire));
+
+    if(isNaN(expire)) {
+      return RESP.encode.error((`ERR: invalid expire time -> ${args[3]}`));
+    }
+
+    if (option === 'PX') {
+      store.set(key, value, new Date(Date.now() + expire));
+    }else if(option === 'EX') {
+      store.set(key, value, new Date(Date.now() + expire * 1000));
+    }
+    
   } else {
     store.set(key, value);
   }
-  
+
   return RESP.encode.simpleString("OK");
 }
